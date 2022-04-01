d946806040b0c94ce563943eba3b093a313c01b0
Yoav Gibri <yoavgibri@gmail.com>
2022-04-01 15:10:13 +0300

    new list filtering

--- app/src/main/java/com/hirshler/remindme/ui/reminderlist/RemindersListViewModel.kt
+++ app/src/main/java/com/hirshler/remindme/ui/reminderlist/RemindersListViewModel.kt
@@ -9,7 +9,6 @@ import com.hirshler.remindme.model.Reminder
 import com.hirshler.remindme.room.ReminderRepo
 import kotlinx.coroutines.launch
 import java.util.*
-import kotlin.collections.set
 
 class RemindersListViewModel : ViewModel() {
 
@@ -18,25 +17,42 @@ class RemindersListViewModel : ViewModel() {
 
     private val remindersLiveData = ReminderRepo().getAllLiveData()
     private val observer = Observer<MutableList<Reminder>> { list ->
-        list.apply {
-
-            sortWith(compareByDescending { it.nextAlarmWithSnooze() })
-
-            if (any { reminder -> !reminder.repeat && reminder.nextAlarmWithSnooze() >= Calendar.getInstance().timeInMillis })
-                add(Reminder(isListTitle = true, text = "Active", manualAlarm = Calendar.getInstance().apply { add(Calendar.MINUTE, 1) }.timeInMillis))
-
-            if (any { reminder -> !reminder.repeat && reminder.nextAlarmWithSnooze() < Calendar.getInstance().timeInMillis }) {
-                add(Reminder(isListTitle = true, text = "Dismissed", manualAlarm = -1))
-            }
-
-            if (any { reminder -> reminder.repeat }) {
-                add(Reminder(isListTitle = true, text = "Repeat").apply { weekDays[1] = true })
-            }
 
 
-            sortWith(compareBy({ it.nextAlarmWithSnooze() < Calendar.getInstance().timeInMillis }, { it.repeat }, { !it.isListTitle }))
+        val now = Calendar.getInstance().timeInMillis
+        val newList = mutableListOf<Reminder>()
+
+        list.run {
+            // Active
+            filter { !it.repeat && it.nextAlarmWithSnooze() > now }.sortedBy { it.nextAlarmWithSnooze() }
+                .let {
+                    if (it.isNotEmpty()) {
+                        newList.add(Reminder(isListTitle = true, text = "Active"))
+                        newList.addAll(it)
+                    }
+                }
+
+            //Repeat
+            filter { it.repeat }.sortedBy { it.nextAlarmWithSnooze() }
+                .let {
+                    if (it.isNotEmpty()) {
+                        newList.add(Reminder(isListTitle = true, text = "Repeat"))
+                        newList.addAll(it)
+                    }
+                }
+
+            //Dismissed
+            filter { !it.repeat && it.nextAlarmWithSnooze() < now }.sortedByDescending { it.nextAlarmWithSnooze() }
+                .let {
+                    if (it.isNotEmpty()) {
+                        newList.add(Reminder(isListTitle = true, text = "Dismissed"))
+                        newList.addAll(it)
+                    }
+                }
         }
-        reminders.value = list
+
+
+        reminders.value = newList
     }
 
     init {
